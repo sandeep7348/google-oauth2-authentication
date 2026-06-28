32b69dc041c0015f793a4c4ae415fd98a15a3e2e
sand7348 <bharnisandeep63@gmail.com>
2026-06-28 12:13:26 +0530

    added github

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const express=require("express")
 const morgan=require("morgan")
 const app=express()
 const passport=require("passport")
+
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var GitHubStrategy=require('passport-github').Strategy
 app.use(passport.initialize())
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -12,6 +14,15 @@ passport.use(new GoogleStrategy({
   },
   function(_, __, profile, cb) {
       return cb(null,profile)
+  }
+));
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_USER_URL
+  },
+  function(_, __, profile, cb){
+     return cb(null,profile)
   }
 ));
 app.use(morgan("dev"));
@@ -24,6 +35,30 @@ app.get('/auth/google/callback',
     console.log(req.user)
     res.redirect('/');
   });
+
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/',session:false }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log("github authentication successfull")
+    console.log(req.user)
+    res.redirect('/');
+  });
+
+
+
+
+
+
+
+
+
+
+
 app.get("/",(req,res)=>{
     res.send("Hello Welcome to the Home Page")
 })
